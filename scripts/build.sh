43511b5ce33731bad08967d2a9481dc97190a491
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-27 17:29:54 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 wget -O whitecat.jpg https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg
 git clone https://github.com/akeenz/emr-hive-dataset.git
 #zip both downlaoded files
-zip artifact.zip whitecat.jpg emr-hive-dataset/dataset/sale.sql
+zip artifact.zip whitecat.jpg dataset/sale.sql
