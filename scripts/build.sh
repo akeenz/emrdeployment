fc4747d65c6727979a39b4743258edd3a6d1787f
akinkunmi akindele hammed <ha.akinkunmi@gmail.com>
2021-05-27 18:39:05 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 wget -O ${buildername}_favourite_picture.jpg ${picture_name}
 git clone https://github.com/akeenz/emr-hive-dataset.git
 #zip both downlaoded files
-zip artifact.zip ${buildername}_favourite_picture.jpg emr-hive-dataset/dataset/sale.sql
+zip ${buildername}_artifact.zip ${buildername}_favourite_picture.jpg emr-hive-dataset/dataset/sale.sql
 ls 
